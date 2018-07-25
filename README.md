18ff4b6e8ef5fd12a5eb0c9ec05e2648fb2b03a5
John Wass <wassj@ctc.com>
2018-07-25 00:10:45 -0400

    update example to both read and write

---
+++ README.md
@@ -0,0 +1,6 @@
+pigpio-scala example
+===
+
+usage
+
+`docker run --rm --privileged -u root -v /sys:/sys -v /dev/mem:/dev/mem pigpio-scala-examples:0.1`
--- src/main/scala/pigpio/examples/Callbacks.scala
+++ src/main/scala/pigpio/examples/Callbacks.scala
@@ -1,16 +1,17 @@
 package pigpio.examples
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.Done
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Timers}
 import pigpio.scaladsl.GpioImplicits._
 import pigpio.scaladsl.GpioPin.Listen
 import pigpio.scaladsl.{GpioAlert, _}
 
 import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.{Duration, DurationInt}
 
 /**
- * Simple demonstration of an Actor based GPIO Callback
- */
+  * Simple demonstration of an Actor based GPIO Callback
+  */
 object Callbacks extends App {
   println("Example of reading callbacks from pin #4")
 
@@ -18,30 +19,49 @@ object Callbacks extends App {
   implicit val lgpio = PigpioLibrary.INSTANCE
 
   // initialize pigpio
-  val ver = lgpio.gpioInitialise
+  val ver = lgpio.gpioInitialise()
   println(s"Initialized pigpio v$ver")
 
-  // create a pin
-  val pin4 = system.actorOf(GpioPin.props(4))
+  // create io pins
+  val in = system.actorOf(GpioPin.props(4))
+  val out = system.actorOf(GpioPin.props(3))
 
-  // create a listener
+  // create a listener and subscribe to callbacks
   val listener = system.actorOf(Props[PrintingActor])
+  in.tell(Listen(), listener)
 
-  // listener subscribes to callbacks
-  pin4.tell(Listen(), listener)
+  // configure pins
+  in ! InputPin
+  out ! OutputPin
 
-  // enable input on the pin
-  pin4 ! InputPin
+  // schedule flipping events on output pin
+  system.actorOf(Flipper.props(out))
 
   Await.ready(system.whenTerminated, Duration.Inf)
 }
 
 /**
- * The Actor receiving the callback
- */
+  * The Actor receiving the callback
+  */
 class PrintingActor extends Actor {
   def receive: Receive = {
     case m: GpioAlert =>
       println(m)
+  }
+}
+
+object Flipper {
+  def props(pin: ActorRef) = Props(new Flipper(pin))
+}
+
+class Flipper(pin: ActorRef) extends Actor with Timers {
+  var level: Level = Low
+
+  timers.startPeriodicTimer("flip", Done, 100.millis)
+
+  def receive: Receive = {
+    case Done ⇒
+      pin ! level
+      level = Level.flip(level)
   }
 }
